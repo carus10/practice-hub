df853951647a353769cfbce6a5d48de3e4512f00
carus10 <taskancihan78@gmail.com>
2026-05-08 18:51:28 +0300

    fix(library): add missing useEffect import in Library component

--- components/Library.tsx
+++ components/Library.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import { Book, BookMode, ProcessingState } from '../types';
 import { extractTextFromPdf, extractCoverFromPdf } from '../services/pdfService';
 import { IconPlus, IconBook, IconTrash, IconUpload, IconRepeat, IconDictionary, IconStudy, IconLanguage, IconNotes } from './Icons';
